0d0e52816474394a98807ef754635d12831e1361
Ananda Raj B <anandraj.baskaran@gmail.com>
2020-10-02 16:51:34 +0530

    Basic Setup

---
+++ Config/Versioning.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceApi.Config
+{
+    internal static class Versioning
+    {
+        internal static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+               {
+                   options.DefaultApiVersion = new ApiVersion(1, 0);
+                   options.AssumeDefaultVersionWhenUnspecified = true;
+               });
+
+            services.AddVersionedApiExplorer(options =>
+                {
+                    // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                    // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                    options.GroupNameFormat = "'v'VVV";
+                    options.DefaultApiVersion = new ApiVersion(1, 0);
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+
+                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                    // can also be used to control the format of the API version in route templates
+                    options.SubstituteApiVersionInUrl = true;
+                });
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceApi.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("ShowName")]
+        public string DisplayName()
+        {
+            return "Its Anandaraj";
+        }
+    }
+}
